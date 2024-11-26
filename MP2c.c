ebb4267f2dc59d817343f2d5bb21e6082cc3e574
Felix Melford Mangawang <159898070+antitripleseven@users.noreply.github.com>
2024-11-26 23:05:56 +0800

    Update MP2c.c

--- MP2c.c
+++ MP2c.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h> 
 
-#define MAX_SIZE 100
+#define MAX_SIZE (1 << 30) 
+#define REPEAT_COUNT 10000 // Number of times to repeat saxpy to amplify execution time
 
 extern void saxpy(int n, float A, float x[], float y[], float z[]);
 
 int main() {
     int n;
     float A;
-    float x[MAX_SIZE];
-    float y[MAX_SIZE];
-    float z[MAX_SIZE];
+    float *x, *y, *z;
 
-    printf("Enter length of array N\n");
+   
+    printf("Enter length of array N (max %d):\n", MAX_SIZE);
     scanf("%d", &n);
 
     if (n > MAX_SIZE) {
@@ -20,28 +21,60 @@ int main() {
         return 1;
     }
 
-    printf("Enter A number\n");
+    
+    x = (float *)malloc(n * sizeof(float));
+    y = (float *)malloc(n * sizeof(float));
+    z = (float *)malloc(n * sizeof(float));
+
+    
+    printf("Enter A number:\n");
     scanf("%f", &A);
 
-    printf("Enter x array\n");
+    
+    printf("Enter x array (size %d):\n", n);
     for (int i = 0; i < n; i++) {
         scanf("%f", &x[i]);
     }
 
-    printf("Enter y array\n");
+   
+    printf("Enter y array (size %d):\n", n);
     for (int i = 0; i < n; i++) {
         scanf("%f", &y[i]);
     }
 
-    saxpy(n, A, x, y, z);
+    // for saxpy kernel
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
 
+    // saxpy kernel for repeated
+    for (int i = 0; i < REPEAT_COUNT; i++) {
+        saxpy(n, A, x, y, z);
+    }
+
+    gettimeofday(&end, NULL);
+    
+    double time_taken = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) * 1e-6;
+
+    
+    double avg_time = time_taken / REPEAT_COUNT;
+
+    // Output the results
     printf("N = %d\n", n);
     printf("A = %f\n", A);
 
+    // Display the first ten elements of the resulting array z
     printf("First ten elements of Z:\n");
     for (int i = 0; i < n && i < 10; i++) {
         printf("%f\n", z[i]);
     }
+
+    printf("Time taken: %.9f seconds\n", time_taken);
+    printf("Average time per saxpy call: %.9f seconds\n", avg_time);
+
+    // Free allocated memory
+    free(x);
+    free(y);
+    free(z);
 
     return 0;
 }
